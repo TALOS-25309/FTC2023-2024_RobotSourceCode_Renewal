7b6a49c1c9795e778be64e850a184850bc7eff44
CLiF <clif1593@gmail.com>
2024-03-21 00:50:08 +0900

    Create AutoWheelPart and Delete Previous Auto Codes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/part/AutoWheelPart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/part/AutoWheelPart.java
@@ -1,12 +1,62 @@
 package org.firstinspires.ftc.teamcode.part;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 // https://www.youtube.com/watch?v=Av9ZMjS--gY
+
+class Odometry {
+    private final DcMotor odometry;
+    private int previous_tick = 0;
+
+    public Odometry(String name, HardwareMap hwm) {
+        this.odometry = hwm.get(DcMotor.class, name);
+        this.odometry.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.odometry.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+    public int getDeltaTick() {
+        int cur_tick = this.odometry.getCurrentPosition();
+        int delta_tick = cur_tick - this.previous_tick;
+        this.previous_tick = cur_tick;
+        return delta_tick;
+    }
+}
+
+class Wheel {
+    private final DcMotor wheel;
+
+    public Wheel(String name, HardwareMap hwm) {
+        this.wheel = hwm.get(DcMotor.class, name);
+        this.wheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+    public void setDirection(DcMotorSimple.Direction direction) {
+        this.wheel.setDirection(direction);
+    }
+    public void move(double power) {
+        this.wheel.setPower(power);
+    }
+    public void stop() {
+        this.wheel.setPower(0);
+    }
+}
+
 public class AutoWheelPart extends Part {
     private double target_x, target_y, target_theta;
+
+    private final Wheel wheelFR, wheelFL, wheelBR, wheelBL;
+    private final Odometry odometryXL, odometryXR, odometryY;
+
+    // Constants
+    // TODO : Change the values
+    private final double X_OFFSET = 1.0;
+    private final double Y_OFFSET = 1.0;
+    private final double TILE_RATIO = 1.0;
+    private final double ABLE_DISTANCE_ERROR = 0.1;
+    private final double ROTATION_SPEED_FACTOR = 0.7; // X length + Y length
 
     private void setTarget(double x, double y, double theta) {
         this.target_x = x;
@@ -19,6 +69,15 @@ public class AutoWheelPart extends Part {
     }
     public AutoWheelPart(HardwareMap hwm, Telemetry tel) {
         super(hwm, tel);
+        this.wheelFR = new Wheel("wheelFR", hwm);
+        this.wheelFL = new Wheel("wheelFL", hwm);
+        this.wheelBR = new Wheel("wheelBR", hwm);
+        this.wheelBL = new Wheel("wheelBL", hwm);
+
+        // TODO : Change the names
+        this.odometryXL = new Odometry("odometryXL", hwm);
+        this.odometryXR = new Odometry("odometryXR", hwm);
+        this.odometryY = new Odometry("odometryY", hwm);
     }
 
     // Set Commands : Positions and Angles
@@ -26,12 +85,68 @@ public class AutoWheelPart extends Part {
     protected void nextStep() {
         RobotCommand cmd = this.current_command;
         if (cmd == Command.MOVE) {
-            setTarget(0, 0, 0);
+            switch(this.step) {
+                case 0:
+                    this.setTarget(0, 0, 0);
+                    break;
+                case 1:
+                    this.finishStep();
+                    break;
+            }
         }
     }
 
     // New Update Function for Autonomous Period
     public void update() {
+        // Position Calculation (By using Odometry)
+        // TODO : Change the formulas (Fit to the robot) + Add IMU
+        double dxl, dxr, dy;
+        dxl = (double)this.odometryXL.getDeltaTick() * TILE_RATIO;
+        dxr = (double)this.odometryXR.getDeltaTick() * TILE_RATIO;
+        dy = (double)this.odometryY.getDeltaTick() * TILE_RATIO;
+
         double cur_x, cur_y, cur_theta;
+        cur_x = (dxl + dxr) / 2.0;
+        cur_y = dy - Y_OFFSET * (dxr - dxl) / 2.0 / X_OFFSET;
+        cur_theta = (dxr - dxl) / 2.0 / X_OFFSET;
+
+        double delta_x, delta_y, delta_theta;
+        delta_x = this.target_x - cur_x;
+        delta_y = this.target_y - cur_y;
+        delta_theta = this.target_theta - cur_theta;
+
+        // Mecanum Wheel Movement Calculation (https://ecam-eurobot.github.io/Tutorials/mechanical/mecanum.html)
+
+        double vx, vy, w;
+        vx = delta_x;
+        vy = delta_y;
+        w = delta_theta;
+
+        double abs_v = Math.abs(vx) + Math.abs(vy) + Math.abs(w * ROTATION_SPEED_FACTOR);
+        if (abs_v > 1.0) {
+            vx /= abs_v;
+            vy /= abs_v;
+            w /= abs_v;
+        }
+
+        this.wheelFL.move(vx - vy - w * ROTATION_SPEED_FACTOR);
+        this.wheelFR.move(vx + vy + w * ROTATION_SPEED_FACTOR);
+        this.wheelBL.move(vx + vy - w * ROTATION_SPEED_FACTOR);
+        this.wheelBR.move(vx - vy + w * ROTATION_SPEED_FACTOR);
+
+        // Check if the robot reached the target
+
+        double ABLE_ANGLE_ERROR = 0.1;
+        if (Math.abs(delta_x) < ABLE_DISTANCE_ERROR
+                && Math.abs(delta_y) < ABLE_DISTANCE_ERROR
+                && Math.abs(delta_theta) < ABLE_ANGLE_ERROR) {
+            this.wheelFL.stop();
+            this.wheelFR.stop();
+            this.wheelBL.stop();
+            this.wheelBR.stop();
+
+            this.step++;
+            this.nextStep();
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/part/WheelPart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/part/WheelPart.java
@@ -7,8 +7,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.hardware.DcMotorHW;
 import org.firstinspires.ftc.teamcode.hardware.DistSensorHW;
-import org.firstinspires.ftc.teamcode.hardware.IMUHW;
-import org.firstinspires.ftc.teamcode.mainop.AutoOpModeLeft;
 
 public class WheelPart extends Part {
     // FR: Front-Right, FL: Front-Left, BR: Back-Right, BL: Back-Left
@@ -24,15 +22,6 @@ public class WheelPart extends Part {
     private double backboard_dist = 0.0;
     private boolean use_auto = false;
 
-    private double autoview_angle;
-    private double autoview_speed;
-    private boolean autoview_running = false;
-
-    public static WheelPart.Direction auto_wheel_move_dir = Direction.Forward;
-    public static int auto_wheel_move_length = 0;
-    public final static int auto_detect_pos_length = 1000;
-
-    public IMUHW imuhw;
     public enum Command implements RobotCommand {
         MOVE_FORWARD,
         MOVE_BACKWARD,
@@ -40,15 +29,7 @@ public class WheelPart extends Part {
         MOVE_RIGHT,
         TURN_LEFT,
         TURN_RIGHT,
-        VIEW_RIGHT,
-        VIEW_LEFT,
-        VIEW_FORWARD,
-        VIEW_BACKWARD,
-        STOP,
-        MOVE_DETECT_POS,
-        AUTO_MOVE,
-        AUTO_LEFT_WITH_SOME_DELAY,
-        AUTO_RIGHT_WITH_SOME_DELAY
+        STOP
     }
 
     public enum Direction {
@@ -96,8 +77,6 @@ public class WheelPart extends Part {
 
         this.backboard_dist_sensor = new DistSensorHW("backboard", hwm, tel);
 
-        this.imuhw = new IMUHW("imu", hwm, tel);
-
         wheelFR.setUsingBrake(true).setUsingEncoder(false).setDirection(DcMotorSimple.Direction.FORWARD);
         wheelFL.setUsingBrake(true).setUsingEncoder(false).setDirection(DcMotorSimple.Direction.REVERSE);
         wheelBR.setUsingBrake(true).setUsingEncoder(false).setDirection(DcMotorSimple.Direction.FORWARD);
@@ -112,7 +91,6 @@ public class WheelPart extends Part {
         this.hardware_manager.registerHardware(this.wheelFR).registerHardware(this.wheelFL);
         this.hardware_manager.registerHardware(this.wheelBR).registerHardware(this.wheelBL);
         this.hardware_manager.registerHardware(this.backboard_dist_sensor);
-        this.hardware_manager.registerHardware(this.imuhw);
     }
 
     public void stop() {
@@ -131,50 +109,11 @@ public class WheelPart extends Part {
 
     public boolean turn = false;
 
-    public void move(double speed, double angle) {
-        this.imuhw.setIsNotFinished();
-        this.autoview_angle = angle;
-        this.autoview_speed = speed;
-        this.autoview_running = true;
-    }
-
-    private void autoview() {
-        double angle = this.autoview_angle;
-        double speed = this.autoview_speed;
-        double currentAngle = imuhw.getAngle();
-        Direction dir;
-        double right = (currentAngle - angle + 360) % 360;
-        double left = (angle - currentAngle + 360) % 360;
-        double level = Math.min(left, right);
-
-        if (left < right) {
-            dir = Direction.TurnLeft;
-            // level *= 1.01;
-        } else {
-            dir = Direction.TurnRight;
-            // level *= 0.99;
-        }
-        if (currentAngle != angle) {
-            if (level < 10) {
-                speed *= level / 10;
-                // speed *= level;
-                if (speed < 0.1) speed = 0.1;
-            }
-            move(speed, dir);
-        }
-        if (level < 0.5) {
-            this.imuhw.setIsFinished();
-            this.autoview_running = false;
-            stop();
-        }
-    }
-
     public void move(double speed, Direction dir, int ticks) {
         this.wheelFL.move(speed * dir.get_value().front_left, ticks);
         this.wheelFR.move(speed * dir.get_value().front_right, ticks);
         this.wheelBL.move(speed * dir.get_value().back_left, ticks);
         this.wheelBR.move(speed * dir.get_value().back_right, ticks);
-
     }
 
     public void moveFreely(double x, double y) {
@@ -196,15 +135,15 @@ public class WheelPart extends Part {
 
     public void update(double linear_length) {
         super.update();
-        double D2 = this.length_of_pincer;
-        double D3 = this.length_of_robot;
-        double d2 = linear_length * Math.cos(Math.toRadians(this.angle_of_linear));
-        double d1 = (linear_length * Math.sin(Math.toRadians(this.angle_of_linear)) + this.height_of_wheel)
-                / Math.tan(Math.toRadians(this.angle_of_backboard));
-        this.backboard_dist = D2-D3+d2-d1;
-        double cur_dist = this.backboard_dist_sensor.getDistance();
-
         if (this.use_auto && this.backboard_dist > 2.0){
+            double D2 = this.length_of_pincer;
+            double D3 = this.length_of_robot;
+            double d2 = linear_length * Math.cos(Math.toRadians(this.angle_of_linear));
+            double d1 = (linear_length * Math.sin(Math.toRadians(this.angle_of_linear)) + this.height_of_wheel)
+                    / Math.tan(Math.toRadians(this.angle_of_backboard));
+            this.backboard_dist = D2-D3+d2-d1;
+            double cur_dist = this.backboard_dist_sensor.getDistance();
+
             double range = 0.5;
             if (this.backboard_dist > cur_dist + range) {
                 double f = this.backboard_dist - (cur_dist + range);
@@ -279,53 +218,6 @@ public class WheelPart extends Part {
                     this.finishStep();
                     break;
             }
-        } else if (cmd == WheelPart.Command.VIEW_RIGHT) {
-            switch (this.step) {
-                case 0:
-                    this.move(wheelSpeed, -90.0);
-                    break;
-                case 1:
-                    this.finishStep();
-                    break;
-            }
-        } else if (cmd == WheelPart.Command.VIEW_LEFT) {
-            switch (this.step) {
-                case 0:
-                    this.move(wheelSpeed, 90.0);
-                    break;
-                case 1:
-                    this.finishStep();
-                    break;
-            }
-        } else if (cmd == WheelPart.Command.VIEW_FORWARD) {
-            switch (this.step) {
-                case 0:
-                    this.move(wheelSpeed, 0.0);
-                    break;
-                case 1:
-                    this.finishStep();
-                    break;
-            }
-        } else if (cmd == WheelPart.Command.VIEW_BACKWARD) {
-            switch (this.step) {
-                case 0:
-                    this.move(wheelSpeed, 180.0);
-                    break;
-                case 1:
-                    this.finishStep();
-                    break;
-            }
-        }
-
-        else if (cmd == WheelPart.Command.MOVE_DETECT_POS) {
-            switch (this.step) {
-                case 0:
-                    this.move(wheelSpeed, Direction.Forward, auto_detect_pos_length);
-                    break;
-                case 1:
-                    this.finishStep();
-                    break;
-            }
         }
 
         // Stop
@@ -337,57 +229,11 @@ public class WheelPart extends Part {
                     break;
             }
         }
-
-        else if (cmd == WheelPart.Command.AUTO_MOVE) {
-            switch (this.step) {
-                case 0:
-                    this.move(wheelSpeed, auto_wheel_move_dir, auto_wheel_move_length);
-                    break;
-                case 1:
-                    this.finishStep();
-                    break;
-            }
-        }
-
-        else if (cmd == Command.AUTO_LEFT_WITH_SOME_DELAY) {
-            switch (this.step) {
-                case 0:
-                    this.delayTime(2000);
-                    break;
-                case 1:
-                    this.move(wheelSpeed, auto_wheel_move_dir, auto_wheel_move_length);
-                    break;
-                case 2:
-                    this.move(wheelSpeed, 90.0); // LEFT VIEW
-                    break;
-                case 3:
-                    this.finishStep();
-                    break;
-            }
-        } else if (cmd == Command.AUTO_RIGHT_WITH_SOME_DELAY) {
-            switch (this.step) {
-                case 0:
-                    this.delayTime(2000);
-                    break;
-                case 1:
-                    this.move(wheelSpeed, auto_wheel_move_dir, auto_wheel_move_length);
-                    break;
-                case 2:
-                    this.move(wheelSpeed, -90.0); // RIGHT VIEW
-                    break;
-                case 3:
-                    this.finishStep();
-                    break;
-            }
-        }
     }
 
     @Override
     public void update() {
         super.update();
-        if(this.autoview_running){
-            this.autoview();
-        }
     }
 
     @Override
